cef1974ced744899dfd015d85cd0337e90ffb303
bwaromale <bwaromalelucky@gmail.com>
2023-03-13 13:38:04 +0100

    Created a Model for Users

---
+++ LibraryManagement/Models/DTO/RegisterDTO.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LibraryManagement.Models.DTO
+{
+    public class RegisterDTO
+    {
+        [Required]
+        public string UserName { get; set; }
+        [Required]
+        public string Password { get; set; }
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        public string Role { get; set; } = "User";
+    }
+}
---
+++ LibraryManagement/Models/User.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LibraryManagement.Models
+{
+    public class User 
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int UserId { get; set; }
+        [Required]
+        public string UserName { get; set; }
+        [Required]
+        public string Password { get; set; }
+        [Required]
+        public string Email { get; set; }
+        
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        [Required]
+        public string Role { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime UpdatedTime { get; set; }
+        [Required]
+        public bool Status { get; set; } = true;
+    }
+}
